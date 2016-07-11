606fbcf3aa511a959e9bb247fea4ef40885fd7e1
Alex <Alex@Alexs-MacBook-Pro.local>
2016-07-11 13:06:03 -0700

    First commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Camera} from 'ionic-native';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(private navController: NavController) {
+
+  }
+
+}
