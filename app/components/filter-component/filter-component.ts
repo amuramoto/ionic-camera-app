45cf5b655f41ac204b27c3c3867c9c1fc0e3320a
Alex Muramoto <amuramoto@apache.org>
2016-07-14 18:56:10 -0700

    Begins refactor of filter service

--- app/components/filter-component/filter-component.ts
+++ app/components/filter-component/filter-component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input } from '@angular/core';
 import { FilterService } from '../../providers/filter-service/filter-service';
 /*
   Generated class for the FilterComponent component.
@@ -7,13 +7,16 @@ import { FilterService } from '../../providers/filter-service/filter-service';
   for more info on Angular 2 Components.
 */
 @Component({
-  selector: 'canvas-filter',
+  selector: 'filter',
   templateUrl: 'build/components/filter-component/filter-component.html',
   providers: [FilterService]
 })
 export class FilterComponent {
 
 	@ViewChild("canvas") filterCanvas: ElementRef;
+	
+	@Input () filterName: string;
+
 	private originalCanvas: any;
 	private originalCtx: CanvasRenderingContext2D;
 	private originalCanvasWidth: number;
@@ -23,16 +26,48 @@ export class FilterComponent {
 	private newCanvas: any;
 	private newCtx: CanvasRenderingContext2D;
 
+	private filterMatrices: Object = {
+		sepia: 	[0.393, 0.349, 0.272, 0,
+						 0.769, 0.686, 0.534, 0,
+						 0.189, 0.168, 0.131, 0],
+		grayscale: [0.33, 0.33, 0.33, 0,
+								0.59,0.59, 0.59, 0,
+								0.11, 0.11, 0.11, 0]			
+		}
   constructor(private _filterService: FilterService) {    
     this.originalCanvas = _filterService.getCanvasClone();
     this.originalCtx = this.originalCanvas.getContext("2d")
     this.originalCanvasWidth = this.originalCtx.canvas.width;
     this.originalCanvasHeight = this.originalCtx.canvas.height;
-    this.imageData = this.originalCtx.getImageData(0, 0, this.originalCanvasWidth, this.originalCanvasHeight);
+    // this.imageData = this.originalCtx.getImageData(0, 0, this.originalCanvasWidth, this.originalCanvasHeight);
 
-    this.newCanvas = this.filterCanvas.nativeElement;
+    this.newCanvas = this.filterCanvas.nativeElement;    
     this.newCtx = this.newCanvas.getContext("2d");
+    this.newCtx.canvas.width = this.newCtx.canvas.height = 50;
   };
 
+
+  private applyFilter (filterName) {
+  	let filterMatrix: Array<number> = this.filterMatrices[filterName];
+ 		let imageData = this.originalCtx.getImageData(0, 0, this.originalCanvasWidth, this.originalCanvasHeight);
+ 		let d = imageData.data;
+
+ 		for (var i = 0; i < d.length; i += 8) {
+    
+ 			let red = d[i];
+ 			let green = d[i + 1];
+ 			let blue = d [i +2];
+ 			let alpha = d [i + 3]; 
+
+      d[i] = red*filterMatrix[0] + red*filterMatrix[1] + red*filterMatrix[2] + red*filterMatrix[3];
+  
+      d[i+1] = green*filterMatrix[4] + green*filterMatrix[5] + green*filterMatrix[6] + green*filterMatrix[7];
+  
+      d[i+2] = blue*filterMatrix[8] + blue*filterMatrix[9] + blue*filterMatrix[10] + blue*filterMatrix[11];
+      
+    } 	
+
+    
+  }
 
 }
--- app/pages/detailed-view/detailed-view.ts
+++ app/pages/detailed-view/detailed-view.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {CameraService} from '../../providers/camera-service/camera-service';
 import {FilterService} from '../../providers/filter-service/filter-service';
 import {FilterComponent} from '../../components/filter-component/filter-component'
 import {File} from 'ionic-native';
@@ -17,11 +16,12 @@ import {File} from 'ionic-native';
 export class DetailedViewPage {
 
 	@ViewChild("photo") photo: ElementRef;
-	photoCtx: CanvasRenderingContext2D;
-	photoCanvas: any;
+	canvas: any;
+	ctx: CanvasRenderingContext2D;	
+	filters: Array<string> = ['grayscale', 'sepia'];
 
 src:any;
-  constructor(private _nav: NavController, private _navParams: NavParams, private _cameraService: CameraService, private _filterService: FilterService) {
+  constructor(private _nav: NavController, private _navParams: NavParams, private _filterService: FilterService) {
 
   }
 
@@ -32,8 +32,8 @@ src:any;
   }
 
   private displayPhoto () {
-  	this.photoCanvas = this.photo.nativeElement;
-  	this.photoCtx = this.photoCanvas.getContext("2d");    		
+  	this.canvas = this.photo.nativeElement;
+  	this.ctx = this.canvas.getContext("2d");    		
   	const image = new Image();
 
   	image.onload = () => {
@@ -41,29 +41,33 @@ src:any;
   		let canvasWidth = window.innerWidth;
   		let canvasHeight = (canvasWidth / image.width) * image.height; //match canvas aspect ratio to original image
   	
-  		this.photoCtx.canvas.width = canvasWidth;
-  		this.photoCtx.canvas.height = canvasHeight;
+  		this.ctx.canvas.width = canvasWidth;
+  		this.ctx.canvas.height = canvasHeight;
   		
-  		this.photoCtx.drawImage(image, 0, 0, image.width, image.height,
+  		this.ctx.drawImage(image, 0, 0, image.width, image.height,
   												 0, 0, canvasWidth, canvasHeight);
   	
+  		this._filterService.setOriginalCanvas(this.canvas)
+  		this._filterService.setImage(image);
   	}
 
   	// image.src=this._navParams.get('file_uri');
   	image.src="./test.png"
+
+
   }
 
  
-  private filterImage (filter, ...var_args) {
-	 	let imageData = this.photoCtx.getImageData(0, 0, this.photoCanvas.width, this.photoCanvas.height);
-	  let args = [imageData];
+ //  private filterImage (filter, ...var_args) {
+	//  	let imageData = this.photoCtx.getImageData(0, 0, this.photoCanvas.width, this.photoCanvas.height);
+	//   let args = [imageData];
 	  
-	  for (let i = 2; i < arguments.length; i++) {
-	    args.push(arguments[i]);
-	  }
-	  let imageDataFiltered = this._filterService[filter].apply(null, args);
-	  this.photoCtx.putImageData(imageDataFiltered, 0, 0);
-	}
+	//   for (let i = 2; i < arguments.length; i++) {
+	//     args.push(arguments[i]);
+	//   }
+	//   let imageDataFiltered = this._filterService[filter].apply(null, args);
+	//   this.photoCtx.putImageData(imageDataFiltered, 0, 0);
+	// }
 
 	
 
